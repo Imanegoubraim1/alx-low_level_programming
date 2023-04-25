27dcab0df4b0567e5912027ebd941c536f881978
Imanegoubraim1 <imanegoubraim@gmail.com>
2023-04-25 15:40:15 -0700

    work

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,8 @@
-int _putchar(char c)
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * Header file for all prototypes
+ * used in 0x02-functions_nested_loops
+ */
+int _putchar(char c);
 void print_alphabet(void);
