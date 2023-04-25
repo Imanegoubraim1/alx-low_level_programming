0b9d4142322c641eba405fb4d8c257ed45643238
Imanegoubraim1 <imanegoubraim@gmail.com>
2023-04-25 14:54:41 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_alphabet - function that prints alphabet
+ *
+ * parameter: void function no parameter
+ *
+ * Return: void function no return
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+}
