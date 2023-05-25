5328d8b55fd0883b50742490f990d028c99ef182
Imanegoubraim1 <imanegoubraim@gmail.com>
2023-05-24 18:39:19 -0700

    you'll never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints each array element on a newl
+ * @array: pointer to array
+ * @size: size of array
+ * @action: pointer to function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (x = 0; x < size; x++)
+	{
+		action(array[x]);
+	}
+}
